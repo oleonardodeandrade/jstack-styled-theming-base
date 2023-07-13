6a2e395dccceb6ccff592b0f775b16c534f94b2b
oleonardodeandrade <79455799+oleonardodeandrade@users.noreply.github.com>
2023-07-13 10:43:43 -0300

    refactor: add files for windows and vscode

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
 const path = require('path');
-const HtmlWebpackPlugin =  require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle[hash].js',
-  }, 
+    filename: 'bundle[hash].js'
+  },
   plugins: [
-    new HtmlWebpackPlugin ({
-      template: path.resolve(__dirname, 'public', 'index.html'),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
     }),
     new CleanWebpackPlugin(),
   ],
@@ -20,7 +20,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: 'babel-loader'
       },
       {
         test: /\.scss$/,
@@ -29,16 +29,15 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              modules: true,
+              modules: true
             },
           },
-          'sass-loader',
+          'sass-loader'
         ],
       },
     ],
-    
   },
   devServer: {
     port: 3000,
-  }
+  },
 };
