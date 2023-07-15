199be5bf26e2fb873058d4d0fcea0db2db854474
oleonardodeandrade <79455799+oleonardodeandrade@users.noreply.github.com>
2023-07-14 22:17:42 -0300

    feat: add changing of themes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { ThemeProvider } from "styled-components";
 
 import GlobalStyle from "./styles/global";
@@ -7,11 +7,24 @@ import Layout from "./components/Layout";
 import themes from './styles/themes';
 
 function App() {
+  const [ theme, setTheme] = useState('dark');
+
+  const currentTheme = useMemo(() => {
+    return themes[theme] || themes.dark;
+  }, [theme]);
+
+  function handleToggleTheme() {
+    setTheme(prevState => prevState === 'dark' ? 'light' : 'dark');
+  }
+
   return (
     <>
-      <ThemeProvider theme={themes.dark}>
+      <ThemeProvider theme={currentTheme}>
         <GlobalStyle />
-        <Layout />
+        <Layout 
+        onToggleTheme={handleToggleTheme}
+        selectedTheme={theme}
+        />
       </ThemeProvider>
     </>
   );
