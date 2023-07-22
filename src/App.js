9bfacb6dff4273aaed95f142ca47ed92792a3a65
oleonardodeandrade <79455799+oleonardodeandrade@users.noreply.github.com>
2023-07-22 20:52:44 -0300

    feat: add binding in App class

--- src/App.js
+++ src/App.js
@@ -13,6 +13,14 @@ class App extends React.Component {
     this.state = {
       theme: "dark",
     };
+
+    this.handleToggleTheme = this.handleToggleTheme.bind(this);
+  }
+
+  handleToggleTheme() {
+    this.setState((prevState) => ({
+      theme: prevState.theme === "dark" ? "light" : "dark",
+    }));
   }
 
   render() {
@@ -23,11 +31,7 @@ class App extends React.Component {
         <ThemeProvider theme={themes[theme] || themes.dark}>
           <GlobalStyle />
           <Layout
-            onToggleTheme={() => {
-              this.setState((prevState) => ({
-                theme: prevState.theme === "dark" ? "light" : "dark",
-              }));
-            }}
+            onToggleTheme={this.handleToggleTheme}
             selectedTheme={theme}
           />
         </ThemeProvider>
