5ddae920c9ee3658c1c98576a4bdabe47d87a908
oleonardodeandrade <79455799+oleonardodeandrade@users.noreply.github.com>
2023-07-21 21:25:11 -0300

    refactor: change App function to class

--- src/App.js
+++ src/App.js
@@ -4,30 +4,60 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/global";
 import Layout from "./components/Layout";
 
-import themes from './styles/themes';
+import themes from "./styles/themes";
 
-function App() {
-  const [ theme, setTheme] = useState('dark');
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const currentTheme = useMemo(() => {
-    return themes[theme] || themes.dark;
-  }, [theme]);
-
-  function handleToggleTheme() {
-    setTheme(prevState => prevState === 'dark' ? 'light' : 'dark');
+    this.state = {
+      theme: "dark",
+    };
   }
 
-  return (
-    <>
-      <ThemeProvider theme={currentTheme}>
-        <GlobalStyle />
-        <Layout 
-        onToggleTheme={handleToggleTheme}
-        selectedTheme={theme}
-        />
-      </ThemeProvider>
-    </>
-  );
+  render() {
+    const { theme } = this.state;
+
+    return (
+      <>
+        <ThemeProvider theme={themes[theme] || themes.dark}>
+          <GlobalStyle />
+          <Layout
+            onToggleTheme={() => {
+              this.setState((prevState) => ({
+                theme: prevState.theme === "dark" ? "light" : "dark",
+              }));
+            }}
+            selectedTheme={theme}
+          />
+        </ThemeProvider>
+      </>
+    );
+  }
 }
+
+// function App() {
+//   const [ theme, setTheme] = useState('dark');
+
+//   const currentTheme = useMemo(() => {
+//     return themes[theme] || themes.dark;
+//   }, [theme]);
+
+//   function handleToggleTheme() {
+//     setTheme(prevState => prevState === 'dark' ? 'light' : 'dark');
+//   }
+
+//   return (
+//     <>
+//       <ThemeProvider theme={currentTheme}>
+//         <GlobalStyle />
+//         <Layout
+//         onToggleTheme={handleToggleTheme}
+//         selectedTheme={theme}
+//         />
+//       </ThemeProvider>
+//     </>
+//   );
+// }
 
 export default App;
