1e14ea2b087a6edc1aba12417d12a7fca2bbc62c
oleonardodeandrade <79455799+oleonardodeandrade@users.noreply.github.com>
2023-10-13 14:36:19 -0300

    feat: add proptypes on Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
 export default class Header extends Component {
+  static propTypes = {
+    selectedTheme: PropTypes.string.isRequired,
+    onToggleTheme: PropTypes.func.isRequired,
+  };
+
   render() {
     const { onToggleTheme, selectedTheme } = this.props;
-      return (
-    <Container>
-      <h1>JStack's Blog</h1>
-      <button type="button" onClick={onToggleTheme}>
-      {selectedTheme === 'dark' ? '🌞' : 'Lua'} 
+    return (
+      <Container>
+        <h1>JStack's Blog</h1>
+        <button type="button" onClick={onToggleTheme}>
+          {selectedTheme === "dark" ? "🌞" : "Lua"}
         </button>
-    </Container>
-  );
+      </Container>
+    );
   }
 }
