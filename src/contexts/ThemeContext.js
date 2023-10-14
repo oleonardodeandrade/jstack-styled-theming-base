08fd106342ae93e3f0236237e7cdd4b462efe27b
oleonardodeandrade <79455799+oleonardodeandrade@users.noreply.github.com>
2023-10-14 04:57:41 -0300

    hotfix: changing ThemeProvider to Class

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -2,16 +2,27 @@ import React, { createContext, useState } from "react";
 
 export const ThemeContext = createContext();
 
-export function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState("dark");
+export class ThemeProvider extends React.Component {
+  state = {
+    theme: "dark",
+  };
 
-  function handleToggleTheme() {
-    setTheme(prevState => prevState === "dark" ? "light" : "dark");
+  handleToggleTheme = () => {
+    this.setState((prevState) => ({
+      theme: prevState.theme === "dark" ? "light" : "dark",
+    }));
+  };
+
+  render() {
+    return (
+      <ThemeContext.Provider
+        value={{
+          theme: this.state.theme,
+          handleToggleTheme: this.handleToggleTheme,
+        }}
+      >
+        {this.props.children}
+      </ThemeContext.Provider>
+    );
   }
-
-  return (
-    <ThemeContext.Provider value={{ theme, handleToggleTheme }}>
-      {children}
-    </ThemeContext.Provider>
-  );
 }
