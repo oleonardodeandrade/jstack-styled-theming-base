408f561575e6201d097a78e711a8bfab7d1101a3
oleonardodeandrade <79455799+oleonardodeandrade@users.noreply.github.com>
2023-10-13 21:03:32 -0300

    feat: using ContextAPI to get props

--- src/App.js
+++ src/App.js
@@ -1,62 +1,27 @@
 import React, { useMemo, useState } from "react";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider as StyledThemeProvider } from "styled-components";
 
 import GlobalStyle from "./styles/global";
 import Layout from "./components/Layout";
+import { ThemeProvider, ThemeContext } from "./contexts/ThemeContext";
 
 import themes from "./styles/themes";
 
 class App extends React.Component {
-  state = {
-    theme: "dark",
-    oiTudoBem: true,
-  };
-
-  handleToggleTheme = () => {
-    this.setState((prevState) => ({
-      theme: prevState.theme === "dark" ? "light" : "dark",
-    }));
-  }
-
   render() {
-    const { theme } = this.state;
-
     return (
-      <>
-        <ThemeProvider theme={themes[theme] || themes.dark}>
-          <GlobalStyle />
-          <Layout
-            onToggleTheme={this.handleToggleTheme}
-            selectedTheme={theme}
-          />
-        </ThemeProvider>
-      </>
+      <ThemeProvider>
+        <ThemeContext.Consumer>
+          {({ theme }) => (
+            <StyledThemeProvider theme={themes[theme] || themes.dark}>
+              <GlobalStyle />
+              <Layout />
+            </StyledThemeProvider>
+          )}
+        </ThemeContext.Consumer>
+      </ThemeProvider>
     );
   }
 }
-
-// function App() {
-//   const [ theme, setTheme] = useState('dark');
-
-//   const currentTheme = useMemo(() => {
-//     return themes[theme] || themes.dark;
-//   }, [theme]);
-
-//   function handleToggleTheme() {
-//     setTheme(prevState => prevState === 'dark' ? 'light' : 'dark');
-//   }
-
-//   return (
-//     <>
-//       <ThemeProvider theme={currentTheme}>
-//         <GlobalStyle />
-//         <Layout
-//         onToggleTheme={handleToggleTheme}
-//         selectedTheme={theme}
-//         />
-//       </ThemeProvider>
-//     </>
-//   );
-// }
 
 export default App;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,21 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 
+import { ThemeContext } from "../../contexts/ThemeContext";
 import { Container } from "./styles";
 
 export default class Header extends Component {
-  static propTypes = {
-    selectedTheme: PropTypes.string.isRequired,
-    onToggleTheme: PropTypes.func.isRequired,
-  };
-
   render() {
-    const { onToggleTheme, selectedTheme } = this.props;
     return (
-      <Container>
-        <h1>JStack's Blog</h1>
-        <button type="button" onClick={onToggleTheme}>
-          {selectedTheme === "dark" ? "🌞" : "Lua"}
-        </button>
-      </Container>
+      <ThemeContext.Consumer>
+        {({ theme, handleToggleTheme }) => (
+          <Container>
+            <h1>JStack's Blog</h1>
+            <button type="button" onClick={handleToggleTheme}>
+              {theme === "dark" ? "🌞" : "🌚"}
+            </button>
+          </Container>
+        )}
+      </ThemeContext.Consumer>
     );
   }
 }
