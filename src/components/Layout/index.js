e61c2bd4bd521db9950fda93151057ddea3a668d
oleonardodeandrade <79455799+oleonardodeandrade@users.noreply.github.com>
2023-10-15 08:06:15 -0300

    feat: add nav routes

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import Header from '../Header';
-import Footer from '../Footer';
-import Routes from '../../Routes';
+import Header from "../Header";
+import Footer from "../Footer";
+import Routes from "../../Routes";
+import { BrowserRouter, Link } from "react-router-dom";
+import { Nav } from "./styles";
 
 export default function Layout() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
+      <Nav>
+        <Link to="/">Home</Link>
+        <Link to="/posts">Posts</Link>
+      </Nav>
       <Routes />
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
