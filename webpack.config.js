253b9ea5f85fdd1478eb3691b16a12ea19409167
oleonardodeandrade <79455799+oleonardodeandrade@users.noreply.github.com>
2023-10-15 08:11:12 -0300

    feat: add publicPath on webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle[hash].js'
+    filename: 'bundle[hash].js',
+    publicPath: '/',
   },
   plugins: [
     new HtmlWebpackPlugin({
